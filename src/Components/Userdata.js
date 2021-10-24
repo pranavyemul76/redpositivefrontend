c596c4ccfa7da17bed4cd2ecfdf09c5cd1262412
pranavyemul76 <76551395+pranavyemul76@users.noreply.github.com>
2021-10-24 20:24:25 +0530

    Update Userdata.js

--- src/Components/Userdata.js
+++ src/Components/Userdata.js
@@ -14,19 +14,20 @@ class Userdata extends React.Component{
             email:"",
              phone:"",
              hobbies:"",
-             id:""
+             _id:""
 
         }
        this.handledelete= this.handledelete.bind(this)
        this.handleupdate= this.handleupdate.bind(this);
        this.onlyNumberKey= this.onlyNumberKey.bind(this);
        this.handleemail= this.handleemail.bind(this);
-      this.handlehobbies= this.handlehobbies.bind(this);
-      this.handleusername= this.handleusername.bind(this);
-     this.handleSubmit = this.handleSubmit.bind(this);
+       this.handlehobbies= this.handlehobbies.bind(this);
+       this.handleusername= this.handleusername.bind(this);
+       this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleupdate=(item)=>{
       this.setState({updateData:[item],_id:item._id,username:item.username,phone:item.phone,email:item.email,hobbies:item.hobbies});
+      
 }
     handleemail(event){
       this.setState({email:event.target.value})
@@ -51,9 +52,8 @@ class Userdata extends React.Component{
       
       axios({
         url:"https://forbackend.herokuapp.com/userupdate",
-       method:"PUT",
-     
-       data:{
+        method:"PUT",
+        data:{
         id:this.state._id,
         username:this.state.username,
         phone:this.state.phone,
@@ -68,82 +68,87 @@ class Userdata extends React.Component{
   
     componentDidMount(){
       
-        axios(
-            {
-              url:`https://forbackend.herokuapp.com/userget`,
-              method:"GET",
-              header:{'content-type':'application/json'},
+       axios(
+       {
+       url:`https://forbackend.herokuapp.com/userget`,
+       method:"GET",
+       header:{'content-type':'application/json'},
       
-            }) 
-          .then(res=>{
-            this.setState({userdata:res.data.user})
-           })
-         
-    }
+       }) 
+       .then(res=>{
+       this.setState({userdata:res.data.user})
+       })
+       }
    
-        handledelete=(email_id)=>{
+       handledelete=(email_id)=>{
        const veer= email_id;
        console.log(veer);
        axios({
        url:`https://forbackend.herokuapp.com/deletedata/${veer}`,
        method:"DELETE",
-        })
+       })
         .then(res=>{alert(res.data.messeage)})
-      }
+       }
   
          
 
     render(){
-        const {userdata,updateData}= this.state;
+        const {userdata,_id}= this.state;
         
         return(
            
          <div>
-           <div class="modal fade" id="exampleModalOne" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-           <div class="modal-dialog" role="document">
-            <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="center">
-     {
-    updateData.map((item)=>{ return <div>
-           <form onSubmit={this.handleSubmit} >
-            <div className="user_input">
-     <input type="text" required placeholder="Name" defaultValue={item.username} onChange={this.handleusername}/>
-    
-   </div>
-   <div className="user_input">
-     <input type="text" required minLength="10" name='telephone' maxLength="10" 
-    defaultValue={item.phone} placeholder="Mobile NO" onChange={this.onlyNumberKey}/>
-    
-   </div>
-   <div className="user_input">
-     <input type="email" required placeholder="Email" name="email" defaultValue={item.email}onChange={this.handleemail}/>
-    
-   </div>
-  <div className="user_input">
-     <input type="text" required defaultValue={item.hobbies} placeholder="Hobbies" onChange={this.handlehobbies}/>
-    
-   </div>
-   <div style={{color:"red",textAlign:'center'}}>
-   <input type="submit" value="Update"/>
-
-   </div>
-   </form>
+           <div className="modal fade" id="exampleModalOne" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+             <div className="modal-dialog" role="document">
+               <div className="modal-content">
+                 <div className="modal-header">
+                   <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">&times;</span>
+                    </button>
+                 </div>
+               <div className="center">
+              {
+             userdata.map((item)=>{ 
+             const data = [item._id];
+             const newdata= data.filter((item)=>{
+             return item.includes(_id);
+              })
+             if(newdata.length>0)
+                        {
+                return <div>
+                <form onSubmit={this.handleSubmit} >
+                  <div className="user_input">
+                      <input type="text" required placeholder="Name" defaultValue={item.username} onChange={this.handleusername}/>
+                  </div>
+                  <div className="user_input">
+                  <input type="text" required minLength="10" name='telephone' maxLength="10" 
+                    defaultValue={item.phone} placeholder="Mobile NO" onChange={this.onlyNumberKey}/>
+                  </div>
+                  <div className="user_input">
+                        <input type="email" required placeholder="Email" name="email" defaultValue={item.email}onChange={this.handleemail}/>
+                  </div>
+                <div className="user_input">
+                  <input type="text" required defaultValue={item.hobbies} placeholder="Hobbies" onChange={this.handlehobbies}/>
+                </div>
+                <div style={{color:"red",textAlign:'center'}}>
+                   <input type="submit" value="Update"/>
+                </div>
+        </form>
          </div>
-   })
+         }
+         else{
+           return null
+         }
+        })
         }
   </div>
 </div>
   </div>
 </div>
 
-<table class="table">
-  <thead class="thead-dark">
+<table className="table">
+  <thead className="thead-dark">
     <tr>
     <th scope="col">select</th>
       <th scope="col"> #</th>
@@ -170,7 +175,7 @@ class Userdata extends React.Component{
                 <td> {item.email}</td>
                 <td> {item.hobbies}</td>
                 <td> <button className="btn" onClick={()=>{this.sendmail(item)}}> Send </button>
-                <button type="button" style={{margin:'4px'}} class="btn "  data-toggle="modal" data-target="#exampleModalOne" onClick={()=>{this.handleupdate(item)}}>
+                <button type="button" style={{margin:'4px'}} className="btn "  data-toggle="modal" data-target="#exampleModalOne" onClick={()=>{this.handleupdate(item)}}>
                    Update
                  </button>
                  <button className="btn" onClick={()=>{this.handledelete(item.email)}}> Delete </button>
